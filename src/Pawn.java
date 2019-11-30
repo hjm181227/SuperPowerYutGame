7d3175e1f407180bf06fd6fe85a7716a22257f7f
heojeongmin <heowjdals981227@gmail.com>
2019-11-30 18:37:18 +0900

    .

--- src/Pawn.java
+++ src/Pawn.java
@@ -5,16 +5,19 @@ public class Pawn extends JLabel {
     private int         currentIndex;
     private boolean     isFinished;
     private Image       pawnImg;
-    public Pawn(String img, int x, int y, int width, int height){
+    public int pawnNumber;
+    public Pawn(String img, int x, int y, int width, int height, int number){
         currentIndex = 0;
         isFinished = false;
         setIcon(new ImageIcon(img));
         setBounds(x,y,width,height);
+
+        pawnNumber = number;
     }// constructor
 
     //  get, set
     public int getCurrentIndex(){ return currentIndex; }
-    public boolean getIsFinished(){ return isFinished; }
+    public boolean isFinished(){ return isFinished; }
 
     public void setIndex(int idx){ currentIndex = idx;}
     public void setFinished(boolean a){ isFinished = a; }
