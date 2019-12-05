3ca677cc4dec3526b0763d187c21494f97c95f93
hyxxsj <hggew@naver.com>
2019-12-06 01:13:32 +0900

    Merge branch 'master' of https://github.com/hjm181227/SuperPowerYutGame into hggew

--- src/Yut.java
+++ src/Yut.java
@@ -7,44 +7,45 @@ import java.awt.*;
 public class Yut extends JLabel implements Runnable{
 
     Thread  myThread;
-    int nSleepTime;
-    public JLabel[] lblYut = new JLabel[2];
-    public String[] ImageName = new String[6];
-    public JLabel A;
-
+    int nSleepTime, nResult;
+//    String[] strYut = new String[6];
+    public String[] iconImageName = new String[6];
     public ImageIcon[]    iconYut;
 
     public Yut(){
         super();
-        ImageName[0] = "images/Do.png";
-        ImageName[1] = "images/Gae.png";
-        ImageName[2] = "images/Girl.png";
-        ImageName[3] = "images/Yut.png";
-        ImageName[4] = "images/Mo.png";
-        ImageName[5] = "images/Bdo.png";
+        iconImageName[0] = "images/Do.png";
+        iconImageName[1] = "images/Gae.png";
+        iconImageName[2] = "images/Girl.png";
+        iconImageName[3] = "images/Yut.png";
+        iconImageName[4] = "images/Mo.png";
+        iconImageName[5] = "images/Bdo.png";
 
         iconYut= new ImageIcon[6];
         for( int i=0; i<6; i++)
-            iconYut[i] = new ImageIcon(ImageName[i]);
+            iconYut[i] = new ImageIcon(iconImageName[i]);
 
         nSleepTime = 100;
-//        setIcon(new ImageIcon(ImageName[0]));
-        setIcon(iconYut[5]);
-
-
+        setIcon(iconYut[0]);
 }
     public Yut(String arg){
         super(arg);
     }
 
     private void initInstanceDatas(){
-
         myThread = null;
         nSleepTime = 1000;
     }
 
     public void setSleepTime(int arg)   { nSleepTime = arg; }
     public int getSleepTime()           { return nSleepTime; }
+
+    public void setResult(int arg)  { nResult= arg;}
+    public int getResult()          {return nResult;}
+
+    public void setResultYutImg(int result){
+        setIcon(iconYut[result-1]);
+    }
 
     public void start(){
         myThread = new Thread(this);
@@ -55,22 +56,21 @@ public class Yut extends JLabel implements Runnable{
         if(myThread != null) myThread.stop();
     }
 
+
     @Override
     public void run(){
         for(int i=0;i<6;i++) {
-            //setText(Integer.toString(i));
-            setIcon(new ImageIcon(ImageName[i]));
+            setIcon(new ImageIcon(iconImageName[i]));
             try { myThread.sleep(nSleepTime); }
             catch (Exception e) { }
         }
-      //  setIcon(new ImageIcon(ImageName[2]));
+
+        setIcon(new ImageIcon(iconImageName[nResult-1]));
+
 
     }//run()
 
-    public void setResultYutImg(int result){
-        setIcon(iconYut[result]);
-        System.out.println(result-1);
-    }
+
 
 
 }//LabelThread Class
